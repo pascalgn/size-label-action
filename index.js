636d46105ebe2f6409c99258f7d42764137ed6a0
Derek Williams <MrFrydae@gmail.com>
2024-05-07 21:24:38 -0400

    Change label name

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ function getSizeLabel(changedLines, sizes = defaultSizes) {
   let label = null;
   for (const lines of Object.keys(sizes).sort((a, b) => a - b)) {
     if (changedLines >= lines) {
-      label = `size/${sizes[lines]}`;
+      label = `Size: ${sizes[lines]}`;
     }
   }
   return label;
@@ -194,7 +194,7 @@ function getLabelChanges(newLabel, existingLabels) {
   const remove = [];
   for (const existingLabel of existingLabels) {
     const { name } = existingLabel;
-    if (name.startsWith("size/")) {
+    if (name.startsWith("Size: ")) {
       if (name === newLabel) {
         add.pop();
       } else {
